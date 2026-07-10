5171709204596e1ed0acf363a254f716256055bf
Cipiraj Rangaraj Parimaladevi(UST <310971@ust.com>
2026-07-10 12:18:29 +0530

    Inject Failure to get Categories 4

--- Week6_Gradle/src/test/java/com/example/Selenium/repository/OrderRepository.java
+++ Week6_Gradle/src/test/java/com/example/Selenium/repository/OrderRepository.java
@@ -92,14 +92,13 @@ public class OrderRepository {
     }
 
   public void resetMutableTables() {
-      String sql = "TRUNCATE retail_order_items, retail_orders RESTART IDENTITY CASCADE";
         try (Connection connection = connection();
              Statement statement = connection.createStatement()) {
-               statement.execute(sql);
-//            statement.execute("SET FOREIGN_KEY_CHECKS = 0");
-//            statement.execute("TRUNCATE TABLE retail_order_items");
-//            statement.execute("TRUNCATE TABLE retail_orders");
-//            statement.execute("SET FOREIGN_KEY_CHECKS = 1");
+
+            statement.execute("SET FOREIGN_KEY_CHECKS = 0");
+            statement.execute("TRUNCATE TABLE retail_order_items");
+            statement.execute("TRUNCATE TABLE retail_orders");
+            statement.execute("SET FOREIGN_KEY_CHECKS = 1");
  
         } catch (SQLException e) {
             throw new IllegalStateException("Could not reset order test data", e);
